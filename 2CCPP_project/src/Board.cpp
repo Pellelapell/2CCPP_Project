853c734587c50534fce64427d029161e882d0a15
Pellelapell <timdem@outlook.fr>
2025-11-08 20:08:11 +0100

    Refactor display logic in Board class for improved readability

--- 2CCPP_project/src/Board.cpp
+++ 2CCPP_project/src/Board.cpp
@@ -7,12 +7,30 @@ using namespace std;
 #include "Board.hpp"
 
     void Board::display()
+    {
+        if (size == 20)
         {
             for (int i = 0; i < size; i++)
-            {
-                std::cout<<".";
-            }
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        std::cout << "....................";
+                    }
+                    std::cout << std::endl;
+                }
         }
+        else 
+        {
+            for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        std::cout << "..............................";
+                    }
+                    std::cout << std::endl;
+                }
+        }
+    }
         
     void Board::defineSize(int numPlayers)
         {
