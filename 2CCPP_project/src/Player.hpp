74827686e9e575c047ce2a3c93887b9f2f11b6fe
Pellelapell <timdem@outlook.fr>
2025-11-09 01:00:18 +0100

    Refactor Player class to use std::string and remove unnecessary using directive

--- 2CCPP_project/src/Player.hpp
+++ 2CCPP_project/src/Player.hpp
@@ -1,11 +1,12 @@
+#pragma once
+#include <string>
 
-using namespace std;
 class Player
 {
 public:
-    string name;
-    string colorName;
-    string colorCode;
+    std::string name;
+    std::string colorName;
+    std::string colorCode;
 
     void assignColorToColorName();
     std::string getColoredName();
